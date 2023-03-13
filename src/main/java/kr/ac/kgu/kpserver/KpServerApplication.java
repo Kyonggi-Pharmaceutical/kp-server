21bd3d3f1efabbeb6d851c6b9d8a7a3fc60b8922
pug <pug.gg@kakaocorp.com>
2023-03-13 21:34:28 +0900

    init project

---
+++ src/main/java/kr/ac/kgu/kpserver/KpServerApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kgu.kpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kgu/kpserver/KpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kgu.kpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
