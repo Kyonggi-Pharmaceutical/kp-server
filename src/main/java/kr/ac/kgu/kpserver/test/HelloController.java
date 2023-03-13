e205fccf1d63dccc8cf237679cf22d081e7c9bf8
pug <pug.gg@kakaocorp.com>
2023-03-14 00:22:21 +0900

    chore: add logging to ping

--- src/main/java/kr/ac/kgu/kpserver/test/HelloController.java
+++ src/main/java/kr/ac/kgu/kpserver/test/HelloController.java
@@ -1,5 +1,7 @@
 package kr.ac.kgu.kpserver.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/ping")
     public String ping() {
+        log.info("### ping");
         return "pong";
     }
 }
