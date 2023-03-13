65c19adee4fdf2997bf439faf91252090662b5e8
pug <pug.gg@kakaocorp.com>
2023-03-13 22:24:57 +0900

    init: add HelloController for test

---
+++ src/main/java/kr/ac/kgu/kpserver/test/HelloController.java
@@ -0,0 +1,15 @@
+package kr.ac.kgu.kpserver.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
