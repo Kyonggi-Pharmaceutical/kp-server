45722b9d9b03c77c8823af362743b6314938c929
pug <pug.gg@kakaocorp.com>
2023-04-02 21:36:35 +0900

    [OVZC-42] feature: 유저 관련 필드 및 클래스 추가

--- src/main/java/kr/ac/kgu/kpserver/domain/user/User.java
+++ src/main/java/kr/ac/kgu/kpserver/domain/user/User.java
@@ -1,11 +1,13 @@
 package kr.ac.kgu.kpserver.domain.user;
 
+import kr.ac.kgu.kpserver.domain.mbti.MBTI;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -23,6 +25,17 @@ public class User {
     private String lastName;
     private String email;
     private String profileImageUrl;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private LocalDate dateOfBirth;
+    private Double height;
+    private Double weight;
+    @Enumerated(EnumType.STRING)
+    private MBTI mbti;
+    private String exerciseGroup; // TODO - 운동 그룹 enum 으로 관리
+    private Integer stressPoint;
+    private Boolean isSmoking;
+    private Boolean isAlcohol;
 
     public User(String firstName, String lastName, String email, String profileImageUrl) {
         this.firstName = firstName;
