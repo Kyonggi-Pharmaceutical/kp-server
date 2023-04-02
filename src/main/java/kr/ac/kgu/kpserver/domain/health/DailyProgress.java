cc3ceb2ffc105ecd9be99e5d9e770403b937b052
pug <pug.gg@kakaocorp.com>
2023-04-02 22:07:52 +0900

    [OVZC-42] feature: DailyProgress 엔티티 추가

---
+++ src/main/java/kr/ac/kgu/kpserver/domain/health/DailyProgress.java
@@ -0,0 +1,27 @@
+package kr.ac.kgu.kpserver.domain.health;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "daily_progresses")
+@AllArgsConstructor
+@NoArgsConstructor
+public class DailyProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean check;
+
+    // TODO - 목표 연관관계
+    // TODO - 유저 연관관계
+
+}
