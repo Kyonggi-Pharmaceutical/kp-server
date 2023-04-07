0676ab3b15f9c7587704053765ebf44e9f2df503
pug <pug.gg@kakaocorp.com>
2023-04-07 23:58:08 +0900

    [OVZC-46] feature: 인증된 유저의 정보를 받아오는 컨트롤러 메서드 파라미터 주입하도록 개선

--- src/main/java/kr/ac/kgu/kpserver/util/KpExceptionType.java
+++ src/main/java/kr/ac/kgu/kpserver/util/KpExceptionType.java
@@ -6,6 +6,7 @@ public enum KpExceptionType {
 
         NOT_FOUND_USER(HttpStatus.NOT_FOUND, 101, "해당 유저를 찾을 수 없습니다."),
         ALREADY_SIGN_UP(HttpStatus.BAD_REQUEST, 102, "이미 회원가입한 유저 입니다."),
+        AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, 103, "인증에 실패했습니다."),
 
         TEST(HttpStatus.BAD_REQUEST, 999, "ERROR MESSAGE");
 
