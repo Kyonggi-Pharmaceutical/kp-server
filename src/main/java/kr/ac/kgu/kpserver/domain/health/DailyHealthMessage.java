0a47df90c7f538640868482233daec667a873d6d
pug <pug.gg@kakaocorp.com>
2023-04-18 23:48:56 +0900

    [OVZC-58] feature: 건강 메시지 엔티티 추가

---
+++ src/main/java/kr/ac/kgu/kpserver/domain/health/DailyHealthMessage.java
@@ -0,0 +1,32 @@
+package kr.ac.kgu.kpserver.domain.health;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "daily_health_messages")
+@AllArgsConstructor
+@NoArgsConstructor
+public class DailyHealthMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DailyHealthMessageType type;
+
+    @Enumerated(EnumType.STRING)
+    private Personality personality;
+
+    @Size(max = 200)
+    private String content;
+
+}
