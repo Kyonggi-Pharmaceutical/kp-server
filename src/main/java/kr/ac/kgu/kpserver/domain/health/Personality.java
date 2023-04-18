41a01ffad66769ca2aff17b56b800f84986f32d7
pug <pug.gg@kakaocorp.com>
2023-04-18 23:42:33 +0900

    [OVZC-58] refactor: Personality enum value 명사로 수정

--- src/main/java/kr/ac/kgu/kpserver/domain/health/Personality.java
+++ src/main/java/kr/ac/kgu/kpserver/domain/health/Personality.java
@@ -1,5 +1,15 @@
 package kr.ac.kgu.kpserver.domain.health;
 
 public enum Personality {
-    INTROVERT, EXTROVERT
+    INTROVERSION("내향성"), EXTROVERSION("외향성");
+
+    private final String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    Personality(String description) {
+        this.description = description;
+    }
 }
