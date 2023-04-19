44fe82c89ee8ab6fbc146274bd132c2576440bb0
pug <pug.gg@kakaocorp.com>
2023-04-20 01:40:28 +0900

    [OVZC-58] chore: 코드 정리

--- src/main/java/kr/ac/kgu/kpserver/domain/health/message/DailyHealthMessageRepository.java
+++ src/main/java/kr/ac/kgu/kpserver/domain/health/message/DailyHealthMessageRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DailyHealthMessageRepository extends JpaRepository<DailyHealthMessage, Long> {
-    public List<DailyHealthMessage> findByTypeAndPersonality(DailyHealthMessageType type, Personality personality);
+    List<DailyHealthMessage> findByTypeAndPersonality(DailyHealthMessageType type, Personality personality);
 }
