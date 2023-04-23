a3895af98fee65178b7b44a9bba0c5320e4c5d91
leejeongeun98 <je981@naver.com>
2023-04-24 06:59:13 +0900

    [OVZC-49] feature : UserExercise 엔티티 추가 및 비지니스 로직 수정

--- src/main/java/kr/ac/kgu/kpserver/domain/exercise/ExerciseDto.java
+++ src/main/java/kr/ac/kgu/kpserver/domain/exercise/ExerciseDto.java
@@ -14,27 +14,4 @@ public class ExerciseDto {
     private final double calorie;
     private final int duration;
 
-    public ExerciseDto mapToDto(Exercise exercise) {
-        return ExerciseDto.builder()
-                .id(exercise.getId())
-                .name(exercise.getName())
-                .exerciseGroup(exercise.getType())
-                .personality(exercise.getPersonality())
-                .calorie(exercise.getCalorie())
-                .duration(exercise.getDuration())
-                .build();
-    }
-
-    public Exercise mapToEntity(ExerciseDto exerciseDto) {
-
-        Exercise exercise = new Exercise();
-        exercise.setId(exerciseDto.getId());
-        exercise.setName(exerciseDto.getName());
-        exercise.setType(exerciseDto.getExerciseGroup());
-        exercise.setPersonality(exerciseDto.getPersonality());
-        exercise.setCalorie(exerciseDto.getCalorie());
-        exercise.setDuration(exerciseDto.getDuration());
-
-        return exercise;
-    }
 }
