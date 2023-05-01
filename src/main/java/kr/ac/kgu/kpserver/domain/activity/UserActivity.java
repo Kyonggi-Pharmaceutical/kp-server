965203469be2118807f961d93a737ce0e494f62e
pug <pug.gg@kakaocorp.com>
2023-05-01 20:53:09 +0900

    [OVZC-82] feature: UserActivity 추가

---
+++ src/main/java/kr/ac/kgu/kpserver/domain/activity/UserActivity.java
@@ -0,0 +1,30 @@
+package kr.ac.kgu.kpserver.domain.activity;
+
+import kr.ac.kgu.kpserver.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user_activities")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserActivity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
+}
---
+++ src/main/java/kr/ac/kgu/kpserver/domain/activity/UserActivityRepository.java
@@ -0,0 +1,8 @@
+package kr.ac.kgu.kpserver.domain.activity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserActivityRepository extends JpaRepository<UserActivity, Long> {
+}
