2625f2ee466aff1e1c0a0a98836301163e8c4f90
pug <pug.gg@kakaocorp.com>
2023-05-13 17:31:37 +0900

    [OVZC-89] feature: MBTI 랭킹 업데이트 스케쥴러

---
+++ src/main/java/kr/ac/kgu/kpserver/domain/rank/schedule/MBTIRankingScheduler.java
@@ -0,0 +1,30 @@
+package kr.ac.kgu.kpserver.domain.rank.schedule;
+
+import kr.ac.kgu.kpserver.domain.rank.MBTIRankingService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MBTIRankingScheduler {
+
+    private final MBTIRankingService mbtiRankingService;
+
+    public MBTIRankingScheduler(MBTIRankingService mbtiRankingService) {
+        this.mbtiRankingService = mbtiRankingService;
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void createDailyMBTIRanking() {
+        mbtiRankingService.createDailyRanking();
+    }
+
+    @Scheduled(cron = "0 0 0 * * MON")
+    public void createWeeklyMBTIRanking() {
+        mbtiRankingService.createWeeklyRanking();
+    }
+
+    @Scheduled(cron = "0 0 0 1 1/1 *")
+    public void createMonthlyMBTIRanking() {
+        mbtiRankingService.createMonthlyRanking();
+    }
+}
