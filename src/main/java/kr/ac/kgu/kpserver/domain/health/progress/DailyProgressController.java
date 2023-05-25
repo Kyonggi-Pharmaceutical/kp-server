be9cc7881c1de46a1f741796d101b94e5012b09d
pug <pug.gg@kakaocorp.com>
2023-05-25 23:29:17 +0900

    feature: 오늘의 일일 진척도 체크 API 추가

--- src/main/java/kr/ac/kgu/kpserver/domain/health/progress/DailyProgressController.java
+++ src/main/java/kr/ac/kgu/kpserver/domain/health/progress/DailyProgressController.java
@@ -3,6 +3,7 @@ package kr.ac.kgu.kpserver.domain.health.progress;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.ac.kgu.kpserver.domain.health.progress.dto.DailyProgressResponse;
+import kr.ac.kgu.kpserver.domain.health.progress.dto.DailyProgressCheckTodayResponse;
 import kr.ac.kgu.kpserver.domain.user.User;
 import kr.ac.kgu.kpserver.security.UserAuthenticated;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +31,14 @@ public class DailyProgressController {
     public ResponseEntity<List<DailyProgressResponse>> getDailyProgressesUpToLastWeek(User user) {
         List<DailyProgressResponse> dailyProgresses = dailyProgressService.getDailyProgressesUpToLastWeek(user.getId());
         return ResponseEntity.ok(dailyProgresses);
+    }
+
+    @Operation(summary = "오늘의 일일 진척도 체크 여부 조회 API")
+    @UserAuthenticated
+    @GetMapping("/is-check-today")
+    public ResponseEntity<DailyProgressCheckTodayResponse> isCheckToday(User user) {
+        DailyProgressCheckTodayResponse response = dailyProgressService.isCheckToday(user.getId());
+        return ResponseEntity.ok(response);
     }
 
 }
