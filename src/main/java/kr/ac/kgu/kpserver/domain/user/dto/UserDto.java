7eae11778925b6834d72910428c70aa874d924eb
pug <pug.gg@kakaocorp.com>
2023-05-30 20:35:31 +0900

    fix: UserDto

--- src/main/java/kr/ac/kgu/kpserver/domain/user/dto/UserDto.java
+++ src/main/java/kr/ac/kgu/kpserver/domain/user/dto/UserDto.java
@@ -7,32 +7,36 @@ import kr.ac.kgu.kpserver.domain.health.UserAnswer;
 import kr.ac.kgu.kpserver.domain.mbti.MBTI;
 import kr.ac.kgu.kpserver.domain.user.Gender;
 import kr.ac.kgu.kpserver.domain.user.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
-    private final Long id;
-    private final String nickname;
-    private final String firstName;
-    private final String lastName;
-    private final String fullName;
-    private final String email;
-    private final String profileImageUrl;
-    private final Gender gender;
-    private final LocalDate dateOfBirth;
-    private final Double height;
-    private final Double weight;
-    private final MBTI mbti;
-    private final HealthcareType healthcareType;
-    private final Integer stressPoint;
-    private final Boolean isSmoking;
-    private final Boolean isAlcohol;
-    private final BMIResponse bmi;
-    private final UserAnswer userAnswer;
+    private Long id;
+    private String nickname;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String email;
+    private String profileImageUrl;
+    private Gender gender;
+    private LocalDate dateOfBirth;
+    private Double height;
+    private Double weight;
+    private MBTI mbti;
+    private HealthcareType healthcareType;
+    private Integer stressPoint;
+    private Boolean isSmoking;
+    private Boolean isAlcohol;
+    private BMIResponse bmi;
+    private UserAnswer userAnswer;
 
 
     public static UserDto from(User user) {
